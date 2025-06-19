697dcf22dacc69dd945c5e1b4878f45d4df99447
Pablo DevOps <pablo@example.com>
2025-06-19 16:52:58 +0000

    Añadido pipeline generado automáticamente (#6)

---
+++ jenkins/pipelines/pipeline-6.groovy
@@ -0,0 +1,11 @@
+// Pipeline generado automáticamente en ejecución #6
+pipeline {
+    agent any
+    stages {
+        stage('Saludo') {
+            steps {
+                echo 'Hola desde el pipeline número 6!'
+            }
+        }
+    }
+}
